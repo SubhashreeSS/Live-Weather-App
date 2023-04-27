5b473626c5f8f81fa170f3e98bddc78ae9d3722b
SubhashreeSS <subhashree2220@gmail.com>
2023-04-27 20:41:37 +0530

    Initial Commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "9e2005cdf3605e22da666bc645151aa8",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
